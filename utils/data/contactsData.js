edd2aa78fb247f7339ff6e1a5612b17a005479b6
M. Rizwan <muhammad.rizwan.razzaq56@gmail.com>
2025-09-05 03:26:29 +0500

    fix: lottie-react ssr issue with dynamic import

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'codebyrizwan072@gmail.com',
-    phone: '+923066592403',
-    address: 'Lahore, Pakistan',
-    github: 'https://github.com/M-Rizwan-Razzaq',
-    facebook: 'https://www.facebook.com/rizwan.razzaq.7771',
-    linkedIn: 'linkedin.com/in/muhammad-rizwan-861201285/',
-    twitter: 'http://x.com/rizwanrazzaq56',
-    stackOverflow: 'https://stackoverflow.com/users/24687611/muhammad-rizwan',
-    devUsername: "Rizwan508"
-}
+  email: "codebyrizwan072@gmail.com",
+  phone: "+923066592403",
+  address: "Lahore, Pakistan",
+  github: "https://github.com/M-Rizwan-Razzaq",
+  facebook: "https://www.facebook.com/rizwan.razzaq.7771",
+  linkedIn: "linkedin.com/in/muhammad-rizwan-861201285/",
+  twitter: "http://x.com/rizwanrazzaq56",
+  // stackOverflow: 'https://stackoverflow.com/users/24687611/muhammad-rizwan',
+  devUsername: "Rizwan508",
+};
